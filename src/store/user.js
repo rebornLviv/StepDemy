983ec34ee5b45aabe21d988c57a7d45fbc040e90
Vlad <vladprodan6@gmail.com>
2020-01-21 21:27:00 +0200

    changes

--- src/store/user.js
+++ src/store/user.js
@@ -74,12 +74,11 @@ export default {
 
     },
     getters: {
-
         user(state) {
             return state.user
+        
         },
         isUserLoggedIn(state) {
-
             return state.user != null
         }
     }
